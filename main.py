9171da306c6377c34555385b7ac94d8ae3efd4fa
skwita <skwita.max@gmail.com>
2021-04-19 00:01:15 +0300

    check id

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ longpoll_ = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 for event in longpoll_.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-   #Слушаем longpoll, если пришло сообщение то:
-        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
-            if event.from_user: #Если написали в ЛС
-                vk.messages.send( #Отправляем сообщение
-                    user_id=event.user_id,
-                    message='Ваш текст'
-		)
-            elif event.from_chat: #Если написали в Беседе
-                vk.messages.send( #Отправляем собщение
+        print(event.user_id)
+        if event.user_id == 54849868:
+            if event.from_chat:  # Если написали в Беседе
+                vk.messages.send(  # Отправляем собщение
                     chat_id=event.chat_id,
-                    random_id=event.user_id,
-                    message='Ваш текст'
-		)
+                    random_id='',
+                    message=' Андрей лох'
+                )
+        if event.user_id == 205466444:
+            if event.from_chat:  # Если написали в Беседе
+                vk.messages.send(  # Отправляем собщение
+                    chat_id=event.chat_id,
+                    random_id='',
+                    message=' Макс крутой'
+                )
+        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы':  # Если написали заданную фразу
+            if event.from_chat:  # Если написали в Беседе
+                vk.messages.send(  # Отправляем собщение
+                    chat_id=event.chat_id,
+                    random_id='',
+                    message='bruh'
+                )
