64b4fc46c4763f69606ab4f70984bd765a4d1d07
Stoncs <andrej2002322@gmail.com>
2021-04-19 00:02:41 +0300

    фыдвоаджыв

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
 from config import token
 
+prob = 3
 vk_session = vk_api.VkApi(token=token)
 
 longpoll_ = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 for event in longpoll_.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-   #Слушаем longpoll, если пришло сообщение то:
-        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
-            if event.from_user: #Если написали в ЛС
-                vk.messages.send( #Отправляем сообщение
-                    user_id=event.user_id,
-                    message='Ваш текст'
-		)
-            elif event.from_chat: #Если написали в Беседе
-                vk.messages.send( #Отправляем собщение
-                    chat_id=event.chat_id,
-                    random_id=event.user_id,
-                    message='Ваш текст'
-		)
+        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы':
+            vk.messages.send(  # Отправляем собщение
+                chat_id=event.chat_id,
+                random_id=get_random_id(),
+                message='Ваш текст'
+            )
